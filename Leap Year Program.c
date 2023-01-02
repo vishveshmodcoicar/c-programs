318888c8b1c24b33caeb9d6427e11cdd52f9669b
vishveshmodcoicar <vishveshmodcoicar@gmail.com>
2023-01-02 13:25:14 +0400

    Create Leap Year Program.c
    a simple program to find a leap year using user input

---
+++ Leap Year Program.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int yr=0;
+    printf("Enter Your Year");
+    scanf("%d",&yr);//lets the user input the year to check...
+    if(yr%4==0){
+        printf("Its A Leap Year");
+            }
+            else
+                printf("Not A Leap Year");
+            }
