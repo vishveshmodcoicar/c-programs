68cb4c98e5ce0359d77d509160c332a962db7d40
vishveshmodcoicar <118298274+vishveshmodcoicar@users.noreply.github.com>
2023-04-19 12:30:22 +0530

    find a pseudo-random number from given user-input

---
+++ Pseudo-Random Number Finder Program.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main( int argc, char *argv[]){
+    
+    if(argc < 2)
+    {
+        printf("Wrong Number Of Inputs");
+        return -1;
+    }
+    int i;
+    int n=atoi(argv[1]);
+    for (i=0 ; i<n ; i++){
+        printf("Pseudo-Random Number: %d\n",(rand() % 80));
+    }
+    return 0;
+}
