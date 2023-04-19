debe18b51b594802e9e4dc69da179c39ba04820a
vishveshmodcoicar <118298274+vishveshmodcoicar@users.noreply.github.com>
2023-04-19 12:28:54 +0530

    find the count of odd & even numbers

---
+++ Odd & Even Counter Program.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+	if (argc < 2){
+		printf("Error");
+		return -1;
+	}
+	int n=atoi(argv[1]);
+	int a[100];
+	int count = 0;	
+	printf("Enter the %d integers: ",n);
+	for(int i=1;i<=n;i++)
+		scanf("%d",&a[i]);
+	printf("\nEven numbers: ");
+	for(int i=1;i<=n;i++)
+	{
+		if(a[i]%2==0)
+		{
+			printf("\n%d",a[i]);
+			count++;
+		}
+	}
+	if(count==0)
+		printf("\nNo Even numbers.");
+	count=0;
+	printf("\nOdd numbers: ");
+	for(int i=1;i<=n;i++)
+	{
+		if(a[i]%2!=0)
+		{
+			printf("\n%d",a[i]);
+			count++;
+		}
+	}
+	if(count==0){
+		printf("\nNo Odd numbers.");
+	printf("\nThe End.");
+	}
+	return 0;
+
+}
